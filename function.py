f86ef2e6289c79ec56f75dfe6779e294b3a652f8
RUS <topvoin@mail.ru>
2024-04-03 00:09:47 +0500

    add func

--- function.py
+++ function.py
@@ -1,20 +1,56 @@
 import json
+import datetime
 
 
 def load_operations():
-    with open('operations.json' 'r') as file:
+    with open('operations.json', 'r') as file:
         data = json.load(file)
     return data
 
 
-def display_last_operations(operations):
-    executed_operations = [op for op in operations if op['state'] == 'EXECUTED']
-    last_executed_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)[:5]
+def hide_cards_number(raw_card):
+    part_1 = raw_card[0:4]
+    part_2 = raw_card[4:6] + '**'
+    part_3 = '****'
+    part_4 = raw_card[12:16]
+    return f'{part_1} {part_2} {part_3} {part_4}'
+
+
+def hide_invoice_number(invoice_number):
+    return f'**{invoice_number[-4:]}'
+
+
+def hide_numbers(value: str):
+    ident, number = value.rsplit(' ', 1)
+    if "счет" in ident.lower():
+        secret_number = hide_invoice_number(number)
+    else:
+        secret_number = hide_cards_number(number)
+    return f"{ident} {secret_number}"
+
+
+def display_last_operations(operations, limit=5):
+    executed_operations = [op for op in operations if op.get('state', '') == 'EXECUTED']
+    last_executed_operations = sorted(
+        executed_operations,
+        key=lambda x: x['date'], reverse=True
+    )[:limit]
 
     for operation in last_executed_operations:
-        print(f"{operation['date']} {operation['description']}")
+        date = datetime.datetime.fromisoformat(operation['date']).date().strftime('%Y.%m.%d')
+        to_operation = hide_numbers(operation['to'])
+
+        print(f"{date} {operation['description']}")
         if 'from' in operation:
-            print(f"{operation['from']} -> {operation['to']}")
+            from_operation = hide_numbers(operation['from'])
+            print(f"{from_operation} -> {to_operation}")
         else:
-            print(f"Карта -> {operation['to']}")
-        print(f"{operation['operationAmount']}")
+            print(f" -> {to_operation}")
+
+        amount = operation['operationAmount']['amount']
+        currency = operation['operationAmount']['currency']['name']
+        print(f"{amount} {currency}")
+
+
+if __name__ == '__main__':
+    display_last_operations(load_operations())
